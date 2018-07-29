4be8d63099d25be48b8e839846c042ef774c7c0b
Shawn Renner <shawn.renner@gmail.com>
2018-07-29 11:15:27 -0500

    falling edge interrupt on button works?

--- main/main.ino
+++ main/main.ino
@@ -1,35 +1,37 @@
 int ledPin = 13;
 int sensePin = 8;
-volatile int value = 0;
+volatile int previousValue = 0;
+volatile byte newValue = 1;
+volatile unsigned long counter = 0;
 
 // Install the interrupt routine.
 ISR(INT6_vect) {
-    value = digitalRead(sensePin);
+  newValue = !newValue;
+  counter++;
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Initializing ihandler");
+  //Serial.println("Initializing ihandler");
   pinMode(ledPin, OUTPUT);
-  pinMode(sensePin, INPUT);
-  Serial.println("Processing initialization");
+  pinMode(sensePin, INPUT_PULLUP);
+  //Serial.println("Processing initialization");
 
-  // Global Enable INT0 interrupt
-  EIMSK |= ( 1 << INT6);
+    
   // Signal change triggers interrupt
-  MCUCR |= ( 1 << ISC00);
-  MCUCR |= ( 0 << ISC01);
+  EICRB |= ( 1 << ISC61);
+  EICRB |= ( 0 << ISC60);
+
+  // Global Enable INT6 interrupt
+  EIMSK |= ( 1 << INT6);
+
+  
   Serial.println("Finished initialization");
 }
 
 void loop() {
-  if (value) {
-    Serial.println("Value high!");
-    digitalWrite(ledPin, HIGH);
-  } else {
-    Serial.println("Value low!");
-    digitalWrite(ledPin, LOW);
-  }
-  delay(100);
+  //digitalWrite(ledPin, newValue);
+  Serial.println(counter);
+  delay(250);
 }
 
