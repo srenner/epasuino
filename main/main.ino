43df6605347d3e01248a47e22f53441592cf44ee
Shawn Renner <shawn.renner@gmail.com>
2018-08-15 22:01:09 -0500

    add potentiometer input

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,9 @@ byte const SPEED_CALC_INTERVAL = 125;                       //read number of pul
 byte const BUFFER_LENGTH = 4;                               //store this instead of always calculating it
 float mphBuffer[BUFFER_LENGTH] = {0.0f, 0.0f, 0.0f, 0.0f};  //keep buffer of mph readings (approx .5 second)
 byte bufferIndex = 0;
+byte const KNOB_PIN = 14;                                   //a0
+int knobPosition = 0;
+int knobBuffer[BUFFER_LENGTH] = {0,0,0,0};                  //keep buffer of potentiometer readings (approx .5 second)
 
 //interrupt routine for interrupt 7 (pin 9)
 ISR(INT7_vect) {
@@ -23,7 +26,7 @@ ISR(INT7_vect) {
 void setup() {
   Serial.begin(9600);
   pinMode(VSS_PIN, INPUT_PULLUP);
-  pinMode(MODE_PIN, INPUT);    
+  pinMode(MODE_PIN, INPUT);
   //set trigger for interrupt 7 (pin 9) to be falling edge (see datasheet)
   EICRB |= ( 1 << ISC71);
   EICRB |= ( 0 << ISC70);
@@ -40,11 +43,13 @@ void loop() {
 
   automaticMode = digitalRead(MODE_PIN);
 
-  Serial.println(automaticMode);
+
   
   //perform speed calculation on an interval of SPEED_CALC_INTERVAL
   if(currentMillis - lastMillis >= SPEED_CALC_INTERVAL && currentMillis > 500) {
 
+
+    
     long pulses = vssCounter - vssCounterPrevious;
     vssCounterPrevious = vssCounter;
 
@@ -70,6 +75,18 @@ void loop() {
     //Serial.println(smoothedMPH);
     sendToCan(smoothedMPH);
     lastMillis = currentMillis;
+
+
+    knobPosition = analogRead(KNOB_PIN);
+    knobBuffer[bufferIndex] = knobPosition;
+    int knobSum = 0;
+    for(int i = 0; i < BUFFER_LENGTH; i++) {
+      knobSum += knobBuffer[i];
+    }
+    int smoothedKnobPosition = knobSum / BUFFER_LENGTH;
+
+    
+    Serial.println(smoothedKnobPosition);
   }
 }
 
